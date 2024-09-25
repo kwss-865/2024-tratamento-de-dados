5b23cd23b84403a0629ffc3e67a6b6402a92cbd8
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-09-25 12:08:18 -0300

    Add files via upload

--- grafico/DadosGerais.js
+++ grafico/DadosGerais.js
@@ -1,1 +1,24 @@
 const url =' https://raw.githubusercontent.com/kwss-865/API_.../refs/heads/main/entrevista.json'
+
+async function vizualizarInformacoes() {
+    const res = await fetch(url)
+    const dados = await res.json()
+
+    const consoleNome = dados.entrevista.consoles[0].nome
+    const consoleVendas = dados.entrevista.consoles[0].vendas 
+    const consoleOpiniao = dados.entrevista.consoles[0].opiniao[0].comentario
+    
+    console.log(consoleOpiniao)
+
+    const paragrafo = document.createElement('p')
+
+    paragrafo.classList.add('caixa-grafico__texto')
+
+    paragrafo.innerHTML = `Em busca de saber os consoles mais vendidos no mundo, foi realizada uma série de pesquisas de diferentes fontes. Com auxílio do ChatGPT, foi possível estimar que o console mais vendido foi ${consoleNome} com um total de aproximadament um total de ${consoleVendas}, com a opinião popular ${consoleOpiniao}`
+
+
+    const caixa = document.getElementById('caixa-grafico')
+    caixa.appendChild(paragrafo)
+}
+
+vizualizarInformacoes()
